94bbd571b8631d29bd0ed9ff89ca5242f4222e34
haydgraham <samuelh.graham@gmail.com>
2023-02-07 18:22:32 -0700

    files for Scripture, Reference, and Word classes.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,8 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Console.WriteLine("something");
     }
 }
