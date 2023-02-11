78ba4f40f8694271f8098b869f1a0e872aa2981b
haydgraham <samuelh.graham@gmail.com>
2023-02-11 14:56:13 -0700

    working

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -8,22 +8,18 @@ class Program
 
         string reference1 = "Proverbs 3:5-6";
         string scripture1 = "Trust in the Lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge him, and he shall direct thy paths.";
-        //call the scripture function to display the scripture for the user to see
-        Console.WriteLine("\n" + Scripture.DisplayScripture(reference1, scripture1) + "\n");
-
-
-        //prompt the user to hit the enter key to hide words
-        Console.WriteLine("\nPress enter to continue or type 'quit' to finish: ");
 
         //main program loop for retrieving user input (Enter/Quit).
         string userInput = "";
+        int numberOfWords = 0;
+        var wordHider = new WordHider(scripture1);
         while(userInput != "quit") {
-            userInput = Console.ReadLine();
-
-            Console.Clear();
-            Console.WriteLine("\n" + Word.HideWord(Word.HideWord(Word.HideWord(Scripture.DisplayScripture(reference1, scripture1) + "\n"))));
+            Console.WriteLine("numberOfWords: " + numberOfWords);
+            Console.WriteLine("\n" + Scripture.DisplayScripture(reference1, wordHider.HideWords(numberOfWords)) + "\n");
             Console.WriteLine("\nPress enter to continue or type 'quit' to finish: ");
-
+            userInput = Console.ReadLine();
+            Console.Clear();
+            numberOfWords++;
         }
         Console.WriteLine("Goodbye!");
     }
