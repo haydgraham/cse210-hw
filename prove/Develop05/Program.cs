46de755b6a39a5d452df3ef82e1ee1b0d3583228
haydgraham <samuelh.graham@gmail.com>
2023-03-11 16:39:18 -0700

    added remove goal feature

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -8,11 +8,11 @@ class Program
         ArrayList goalHolder = new ArrayList();
         ArrayList goalHolderCsv = new ArrayList();
 
-        string menu = "Menu options:\n 1. Create New Goal\n 2. List Goals\n 3. Save Goals\n 4. Load Goals\n 5. Record Event\n 6. Quit\nSelect a choice from the menu:";
+        string menu = "Menu options:\n 1. Create New Goal\n 2. List Goals\n 3. Save Goals\n 4. Load Goals\n 5. Record Event\n 6. Remove Goal\n 7. Quit\nSelect a choice from the menu:";
         Console.WriteLine(menu);
 
         string choice = "";
-        while(choice != "6"){
+        while(choice != "7"){
             choice = Console.ReadLine();
             if(choice == "1"){
                 Console.WriteLine("The types of goals are:\n 1. Simple Goal\n 2. Eternal Goal\n 3. Checklist Goal\nWhich type of goal would you like to create?");
@@ -109,6 +109,8 @@ class Program
 
                         Simple simple = new Simple(completed, name, description, points);
                         goalHolder.Add(simple.ToGoalFormat());
+                        goalHolderCsv.Add(simple.ToCsvFormat());
+
                     }
                     else if(goalType == "Eternal"){
                         Boolean completed = bool.Parse(parts[1]);
@@ -118,6 +120,8 @@ class Program
 
                         Eternal eternal = new Eternal(name, description, points);
                         goalHolder.Add(eternal.ToGoalFormat());
+                        goalHolderCsv.Add(eternal.ToCsvFormat());
+
                     }
                     else if(goalType == "Checklist"){
                         Boolean completed = bool.Parse(parts[1]);
@@ -129,14 +133,10 @@ class Program
 
                         Checklist checklist = new Checklist(completed, name, description, points, times, bonus);
                         goalHolder.Add(checklist.ToGoalFormat());
+                        goalHolderCsv.Add(checklist.ToCsvFormat());
                     }
                 }
-                Console.WriteLine("The goals are:");
-                int i = 1;
-                foreach(string goal in goalHolder){
-                    Console.WriteLine($" {i}. {goal}");
-                    i++;
-                }
+               
                 Console.WriteLine($"\n{menu}");
 
             }
@@ -152,6 +152,14 @@ class Program
 
                 Console.WriteLine(menu);
             }
+            else if(choice == "6"){
+                Console.WriteLine("Which goal would you like to remove?");
+                int goalIndex = int.Parse(Console.ReadLine()) - 1;
+                goalHolder.RemoveAt(goalIndex);
+                goalHolderCsv.RemoveAt(goalIndex);
+
+                Console.WriteLine(menu);
+            }
+            }
         }
     }
-}
